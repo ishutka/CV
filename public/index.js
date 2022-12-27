0ee25c0cd8a8933af474415fa49bfe09626e8f2f
inquisitive <inquisitive@syneforge.com>
2022-12-27 16:03:14 +0200

    fix experience date

--- public/index.js
+++ public/index.js
@@ -1,11 +1,23 @@
 const asideTitles = document.querySelectorAll("aside h3");
 
-var i = 0;
-for (i; i < asideTitles.length; i++) {
-  const title = asideTitles[i];
-  title.addEventListener("click", function() {
-    if (window.innerWidth < 601) {
-      title.parentElement.classList.toggle("open");
-    }
-  });
+if (asideTitles.length)
+  for (let i = 0; i < asideTitles.length; i++) {
+    const title = asideTitles[i];
+
+    title.addEventListener("click", function() {
+      if (window.innerWidth < 601) {
+        title.parentElement.classList.toggle("open");
+      }
+    });
+  }
+
+const experience = document.querySelector('#experience');
+
+if (experience) {
+  let date = new Date();
+  const startYear = new Date('2018-01-01');
+  const wholeYears = (date - startYear) /1000/3600/24/36.435;
+
+  experience.innerHTML = Math.trunc(wholeYears)/ 10;
+  experience.classList.add('activated');
 }
